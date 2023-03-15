da9b562e27308fe6a995a5fb053766d734ae4720
Artem <kutuavin2@gmail.com>
2023-03-15 18:28:30 +0500

    install Room

---
+++ app/src/main/java/com/example/shoppinglist/data/AppDataBase.kt
@@ -0,0 +1,44 @@
+package com.example.shoppinglist.data
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [ShopItemDbModel::class], version = 1, exportSchema = false)
+abstract class AppDataBase: RoomDatabase() {
+
+    abstract fun shopListDao(): ShopListDao
+
+    companion object{
+        private var INSTANCE: AppDataBase? = null
+        private val LOCK = Any()
+        private const val DB_NAME = "shop_item.db"
+
+        fun getInstance(application: Application): AppDataBase{
+             var dataBase: AppDataBase
+
+            INSTANCE?.let{
+                dataBase = it
+            }
+
+            synchronized(LOCK){
+                INSTANCE?.let {
+                    dataBase = it
+                }
+
+                 dataBase = Room.databaseBuilder(
+                    application,
+                 AppDataBase::class.java,
+                    DB_NAME
+                ).allowMainThreadQueries()
+                     .build()
+
+                INSTANCE= dataBase
+                return dataBase
+            }
+
+        }
+    }
+}
